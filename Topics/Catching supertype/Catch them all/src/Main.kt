cebbf5c77f17182040cd24547a94531baa72cda2
ColdCoder92 <coldcoder9225@pm.me>
2025-04-02 21:13:20 -0400

    Add Catching Supertype Exceptions

---
+++ Topics/Catching supertype/Catch them all/src/Main.kt
@@ -0,0 +1,19 @@
+// Do not change the code below
+open class BaseException : Exception()
+open class MidException : BaseException()
+class DerivedException : MidException()
+
+fun main() {
+    val exceptionList = listOf(DerivedException(), MidException(), BaseException())
+    for (ex in exceptionList) {
+        try {
+            throw ex
+        } catch (e: Exception) { // Write the code to handle each exception as recommended
+            when (e) {
+                is DerivedException -> println("Caught a DerivedException!")
+                is MidException -> println("Caught a MidException!")
+                is BaseException -> println("Caught a BaseException!")
+            }
+        }
+    }
+}
