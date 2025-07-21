fd99b1277e0497d0ce80e8beffdb5697bfd67566
ColdCoder92 <coldcoder9225@pm.me>
2025-07-21 11:44:44 -0400

    Add Mapping Transformations

---
+++ Topics/Mapping transformation/Harry and the Spell Power/src/Main.kt
@@ -0,0 +1,14 @@
+data class Spell(val name: String, val power: Int)
+
+fun main() {
+    val input = readln().split(" ")
+    val spells = input.map { Spell(it.split("-")[0], it.split("-")[1].toInt()) }
+
+    // write your code here
+    val res = spells.mapNotNull { spell ->
+        if (spell.power >= 40) spell.name
+        else null
+    }
+
+    println(res)
+}
