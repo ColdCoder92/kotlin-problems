6d42fc6bd5d11d775fba89bbaa5bbc07116b022c
ColdCoder92 <coldcoder9225@pm.me>
2025-08-15 20:49:13 -0400

    Add "Class Delegation"

---
+++ Topics/Class delegation/Administrator or User/src/Main.kt
@@ -0,0 +1,27 @@
+interface Auth {
+    fun login()
+    fun logout()
+}
+
+class AdminAuth : Auth {
+    override fun login() {
+        println("Admin logged in.")
+    }
+
+    override fun logout() {
+        println("Admin logged out.")
+    }
+}
+
+class UserAuth : Auth {
+    override fun login() {
+        println("User logged in.")
+    }
+
+    override fun logout() {
+        println("User logged out.")
+    }
+}
+
+class SystemUser(val username: String, val isAdmin: Boolean)
+    : Auth by if (isAdmin) AdminAuth() else UserAuth()
